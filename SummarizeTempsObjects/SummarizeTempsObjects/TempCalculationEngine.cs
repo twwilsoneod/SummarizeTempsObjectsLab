aabb9cf7979e1a65254a580b3c22fa803b637418
twwilsoneod <59981796+twwilsoneod@users.noreply.github.com>
2020-03-10 15:46:27 -0500

    Update TempCalculationEngine.cs

--- SummarizeTempsObjects/SummarizeTempsObjects/TempCalculationEngine.cs
+++ SummarizeTempsObjects/SummarizeTempsObjects/TempCalculationEngine.cs
@@ -16,31 +16,93 @@ namespace SummarizeTempsObjects
         public int AverageTemperature()
         {
             // add logic to calculate the average
-            return 0; // <-- Replace this with the actual average
+            int _tempValue = 0;
+            int _tempCount = 0;
+            int _tempAve = 0;
+
+            foreach (int line in _temperatures)
+            {
+                _tempValue += line;
+                _tempCount += 1;
+
+                _tempAve = _tempValue / _tempCount;
+            }
+
+            return _tempAve; // <-- Replace this with the actual average
         }
 
         public int TempsAboveThreshold(int threshold)
         {
             // add logic to calculate temps above the threshold
-            return 0; // <-- Replace this with the number
+            int _tempCountAbove = 0;
+
+            foreach (int line in _temperatures)
+            {
+                if (line > threshold)
+                {
+                    _tempCountAbove += 1;
+                }
+                else
+                {
+
+                }
+            }
+            return _tempCountAbove; // <-- Replace this with the number
         }
 
         public int TempsBelowThreshold(int threshold)
         {
             // add logic to calculate and return temps below the threshold
-            return 0; // <-- Replace this with the number
+            int _tempCountBelow = 0;
+
+            foreach (int line in _temperatures)
+            {
+                if(line < threshold)
+                {
+                    _tempCountBelow += 1;
+                }
+                else
+                {
+
+                }
+            }
+            return _tempCountBelow; // <-- Replace this with the number
         }
 
         public int TempsAtThreshold(int threshold)
         {
             // add logic to calculate and return temps at the threshold
-            return 0; // <-- Replace this with the number
+            int _tempIsThresholdCount = 0;
+
+            foreach(int line in _temperatures)
+            {
+                if(line == threshold)
+                {
+                    _tempIsThresholdCount += 1;
+                }
+                else
+                {
+
+                }
+            }
+            return _tempIsThresholdCount; // <-- Replace this with the number
         }
 
         public int NumberOfTemperatures()
         {
             // add logic to return the number of temparature readings
-            return 0; // <-- Replace this with the number
+
+            int _tempCount = 0;
+
+            foreach (int line in _temperatures)
+
+            {
+                _tempCount += 1;
+            }
+
+            return _tempCount;// <-- Replace this with the number
+            
+            
         }
     }
 }
